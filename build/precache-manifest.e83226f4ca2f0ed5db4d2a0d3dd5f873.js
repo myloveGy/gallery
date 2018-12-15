f11bec837f688077f078274caab5bf8c9b7a862d
jinxing.liu <821901008@qq.com>
2018-12-15 21:59:09 +0800

    项目基本完成

---
+++ build/precache-manifest.e83226f4ca2f0ed5db4d2a0d3dd5f873.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "cab162c2b3c2f2a6f0b664abc2613bc0",
+    "url": "/static/media/iconfont.cab162c2.svg"
+  },
+  {
+    "revision": "adc107f314ccff86160bd5019c8f7487",
+    "url": "/static/media/iconfont.adc107f3.eot"
+  },
+  {
+    "revision": "4e2a4d1348cca26ddf25193f723571ef",
+    "url": "/static/media/iconfont.4e2a4d13.ttf"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "980a30e8c32f8c9b76a5",
+    "url": "/static/js/main.980a30e8.chunk.js"
+  },
+  {
+    "revision": "2516a9b6810f0108e8fe",
+    "url": "/static/js/1.2516a9b6.chunk.js"
+  },
+  {
+    "revision": "980a30e8c32f8c9b76a5",
+    "url": "/static/css/main.357fd8c8.chunk.css"
+  },
+  {
+    "revision": "1a641dee14599cadbfd92f8266b2cc02",
+    "url": "/index.html"
+  }
+];
