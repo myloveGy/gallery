3b3ff0d66bc8749184d8fe86800185a434b58957
jinxing.liu <821901008@qq.com>
2020-01-13 13:31:46 +0800

    feat: 修改默认背景

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,17 +1,14 @@
-import React, {Component} from 'react'
-import images from "../data/image"
-import {ImgFigure, ControllerUnit} from "./index"
+import React, {Component, Fragment} from 'react'
+import images from '../data/image'
+import {ImgFigure, ControllerUnit} from './index'
 import ReactDOM from 'react-dom'
-import {get30DegRandom, getRangeRandom} from "../utils"
+import {get30DegRandom, getRangeRandom} from '../utils'
 
 class Gallery extends Component {
 
-  constructor(props) {
-    super(props)
-    this.images = []
-    this.state = {
-      imagesArrangeArray: [
-        /* {
+  state = {
+    imagesArrangeArray: [
+      /* {
             // 定位信息
             pos: {
                 left: 0,
@@ -26,9 +23,15 @@ class Gallery extends Component {
             // 是否居中
             isCenter: false,
         } */
-      ]
-    }
+    ],
+    starsCount: 800,
+    distance: 800,
   }
+
+  // 图片信息
+  images = []
+
+  stars = []
 
   Constant = {
     centerPos: {
@@ -39,14 +42,14 @@ class Gallery extends Component {
     hPosRange: {
       leftSectionX: [0, 0],
       rightSectionX: [0, 0],
-      y: [0, 0]
+      y: [0, 0],
     },
     // 垂直方法的取值
     vPosRange: {
       x: [0, 0],
-      topY: [0, 0]
-    }
-  };
+      topY: [0, 0],
+    },
+  }
 
   /**
    * 翻转图片
@@ -87,38 +90,47 @@ class Gallery extends Component {
   componentDidMount() {
     // 获取浏览器宽度和高度
     let sDom = ReactDOM.findDOMNode(this.input)
-    let sWidth = sDom.scrollWidth;
-    let sHeight = sDom.scrollHeight;
-    let sHalfWidth = Math.ceil(sWidth / 2);
-    let sHalfHeight = Math.ceil(sHeight / 2);
+    let sWidth = sDom.scrollWidth
+    let sHeight = sDom.scrollHeight
+    console.info(sDom.scrollWidth, sDom.scrollHeight)
+    let sHalfWidth = Math.ceil(sWidth / 2)
+    let sHalfHeight = Math.ceil(sHeight / 2)
 
     // 拿到一个imageFigure 的大小
     let dom = ReactDOM.findDOMNode(this.images[0])
     let iWidth = dom.scrollWidth,
       iHeight = dom.scrollHeight,
       hWidth = Math.ceil(iWidth / 2),
-      hHeight = Math.ceil(iHeight / 2);
+      hHeight = Math.ceil(iHeight / 2)
 
     this.Constant.centerPos = {
       left: sHalfWidth - hWidth,
-      top: sHalfHeight - hHeight
+      top: sHalfHeight - hHeight,
     }
 
     // 计算左侧、右侧的取值范围
-    this.Constant.hPosRange.leftSectionX[0] = -hWidth;
-    this.Constant.hPosRange.leftSectionX[1] = sHalfWidth - hWidth * 3;
-    this.Constant.hPosRange.rightSectionX[0] = sHalfWidth + hWidth;
-    this.Constant.hPosRange.rightSectionX[1] = sWidth - hWidth;
-    this.Constant.hPosRange.y[0] = -hHeight;
-    this.Constant.hPosRange.y[1] = sHeight - hHeight;
+    this.Constant.hPosRange.leftSectionX[0] = -hWidth
+    this.Constant.hPosRange.leftSectionX[1] = sHalfWidth - hWidth * 3
+    this.Constant.hPosRange.rightSectionX[0] = sHalfWidth + hWidth
+    this.Constant.hPosRange.rightSectionX[1] = sWidth - hWidth
+    this.Constant.hPosRange.y[0] = -hHeight
+    this.Constant.hPosRange.y[1] = sHeight - hHeight
 
     // 计算上侧区域图片分布位置的取值范围
-    this.Constant.vPosRange.topY[0] = -hHeight;
-    this.Constant.vPosRange.topY[1] = sHalfHeight - hHeight * 3;
-    this.Constant.vPosRange.x[0] = sHalfWidth - iWidth;
-    this.Constant.vPosRange.x[1] = sHalfWidth;
+    this.Constant.vPosRange.topY[0] = -hHeight
+    this.Constant.vPosRange.topY[1] = sHalfHeight - hHeight * 3
+    this.Constant.vPosRange.x[0] = sHalfWidth - iWidth
+    this.Constant.vPosRange.x[1] = sHalfWidth
 
     this.rearrange(0)
+
+    this.stars.forEach(div => {
+      let speed = 0.2 + Math.random()
+      let thisDistance = 800 + Math.random() * 300
+      div.style.transformOrigin = '0 0 ' + thisDistance + 'px'
+      div.style.transform = 'translate3d(0, 0, -' + thisDistance + 'px) rotateY(' + Math.random() * 360 + 'deg) rotateX(' +
+        Math.random() * -50 + 'deg) scale(' + speed + ', ' + speed + ')'
+    })
   }
 
   /**
@@ -140,7 +152,7 @@ class Gallery extends Component {
       imagesArrangeTopArray = [],
       // 取一个或者不取一个图片放在上面
       topImageNum = Math.floor(Math.random() * 2),
-      imagesArrangeCenterArray = imagesArrangeArray.splice(centerIndex, 1);
+      imagesArrangeCenterArray = imagesArrangeArray.splice(centerIndex, 1)
 
     // 首先居中centerIndex 的图片
     imagesArrangeCenterArray[0] = {
@@ -148,11 +160,11 @@ class Gallery extends Component {
       rotate: 0,
       isInverse: false,
       isCenter: true,
-    };
+    }
 
     // 取出要布局上侧的图片的状态信息
-    let topImageSpliceIndex = Math.ceil(Math.random() * (imagesArrangeArray - topImageNum));
-    imagesArrangeTopArray = imagesArrangeArray.splice(topImageSpliceIndex, topImageNum);
+    let topImageSpliceIndex = Math.ceil(Math.random() * (imagesArrangeArray - topImageNum))
+    imagesArrangeTopArray = imagesArrangeArray.splice(topImageSpliceIndex, topImageNum)
 
     // 布局位于上侧的图片
     imagesArrangeTopArray.forEach((value, index) => {
@@ -169,12 +181,12 @@ class Gallery extends Component {
 
     // 布局左右两侧的图片
     for (let i = 0, j = imagesArrangeArray.length, k = j / 2; i < j; i++) {
-      let hPosRangeLORX = i < k ? hPosRangeLeftSecX : hPosRangeRightSecX;
+      let hPosRangeLORX = i < k ? hPosRangeLeftSecX : hPosRangeRightSecX
 
       imagesArrangeArray[i] = {
         pos: {
           top: getRangeRandom(hPosRangeY[0], hPosRangeY[1]),
-          left: getRangeRandom(hPosRangeLORX[0], hPosRangeLORX[1])
+          left: getRangeRandom(hPosRangeLORX[0], hPosRangeLORX[1]),
         },
         rotate: get30DegRandom(),
         isInverse: false,
@@ -190,11 +202,20 @@ class Gallery extends Component {
     this.setState({imagesArrangeArray})
   }
 
+  renderStars() {
+    let stars = []
+    for (let i = 0; i < this.state.starsCount; i++) {
+      stars.push(<div className={'star'} key={`star-${i}`} ref={d => this.stars.push(d)}/>)
+    }
+
+    return stars
+  }
+
   render() {
-    let imgFigures = [], controllerUnit = [];
+    let imgFigures = [], controllerUnit = []
     images.forEach((item, index) => {
       if (!this.state.imagesArrangeArray[index]) {
-        let imagesArrangeArray = this.state.imagesArrangeArray;
+        let imagesArrangeArray = this.state.imagesArrangeArray
         imagesArrangeArray[index] = {
           pos: {
             left: 0,
@@ -223,19 +244,27 @@ class Gallery extends Component {
       // 按钮信息
       controllerUnit.push(<ControllerUnit key={index} arrange={this.state.imagesArrangeArray[index]}
                                           handle={this.handle(index)}/>)
-    });
+    })
 
     return (
-      <section className="stage" style={{height: window.document.body.scrollHeight + "px"}} ref={input => {
-        this.input = input
-      }}>
-        <section className="img-sec">
-          {imgFigures}
+      <Fragment>
+        <div className={'stars'}>
+          {this.renderStars()}
+        </div>
+
+        <section
+          className="stage"
+          style={{height: document.documentElement.clientHeight + 'px'}}
+          ref={input => this.input = input}
+        >
+          <section className="img-sec">
+            {imgFigures}
+          </section>
+          <nav className="controller-nav">
+            {controllerUnit}
+          </nav>
         </section>
-        <nav className="controller-nav">
-          {controllerUnit}
-        </nav>
-      </section>
+      </Fragment>
     )
   }
 }
--- src/data/image.js
+++ src/data/image.js
@@ -1,57 +1,57 @@
 export default [
   {
     "url": require("../images/1.jpg"),
-    "title": "my name is liujinxing",
+    "title": "my name is jinxing.liu",
     "content": "my name is jinxing"
   },
   {
     "url": require("../images/2.jpg"),
-    "title": "my name is liujinxing",
+    "title": "my name is jinxing.liu",
     "content": "my name is jinxing"
   },
   {
     "url": require("../images/3.jpg"),
-    "title": "my name is liujinxing",
+    "title": "my name is jinxing.liu",
     "content": "my name is jinxing"
   },
   {
     "url": require("../images/4.jpg"),
-    "title": "my name is liujinxing",
+    "title": "my name is jinxing.liu",
     "content": "my name is jinxing"
   },
   {
     "url": require("../images/5.jpg"),
-    "title": "my name is liujinxing",
+    "title": "my name is jinxing.liu",
     "content": "my name is jinxing"
   },
   {
     "url": require("../images/6.jpg"),
-    "title": "my name is liujinxing",
+    "title": "my name is jinxing.liu",
     "content": "my name is jinxing"
   },
   {
     "url": require("../images/7.jpg"),
-    "title": "my name is liujinxing",
+    "title": "my name is jinxing.liu",
     "content": "my name is jinxing"
   },
   {
     "url": require("../images/6.jpg"),
-    "title": "my name is liujinxing",
+    "title": "my name is jinxing.liu",
     "content": "my name is jinxing"
   },
   {
     "url": require("../images/7.jpg"),
-    "title": "my name is liujinxing",
+    "title": "my name is jinxing.liu",
     "content": "my name is jinxing"
   },
   {
     "url": require("../images/3.jpg"),
-    "title": "my name is liujinxing",
+    "title": "my name is jinxing.liu",
     "content": "my name is jinxing"
   },
   {
     "url": require("../images/4.jpg"),
-    "title": "my name is liujinxing",
+    "title": "my name is jinxing.liu",
     "content": "my name is jinxing"
   }
 ];
