97952c86cb2a503682dc1b8f1302e7922b768d4e
myloveGy <821901008@qq.com>
2020-01-13 06:48:11 +0000

    Deploying to gh-pages from master cb5d176c7055bf39c72329672130dd6b54376bce

---
+++ precache-manifest.539b9169cc8ac9b69e6b426bd896d868.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "933fdfdc3f9a3923a2120f62b05a3bab",
+    "url": "./static/media/1.933fdfdc.jpg"
+  },
+  {
+    "revision": "73ee9f6a395bf200078a",
+    "url": "./static/css/main.a9b047f4.chunk.css"
+  },
+  {
+    "revision": "bb37434a3d6d821c272a",
+    "url": "./static/js/1.bb37434a.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "c58ac24a78cee42821d95ad8d0287539",
+    "url": "./static/media/3.c58ac24a.jpg"
+  },
+  {
+    "revision": "c0fcb7dea5fc0a4bbdce48a6de379d5d",
+    "url": "./static/media/4.c0fcb7de.jpg"
+  },
+  {
+    "revision": "ea31353a32b885a5fafa0055395692a1",
+    "url": "./static/media/6.ea31353a.jpg"
+  },
+  {
+    "revision": "73ee9f6a395bf200078a",
+    "url": "./static/js/main.73ee9f6a.chunk.js"
+  },
+  {
+    "revision": "682a6ba0869df298e977eee48dfd3920",
+    "url": "./static/media/2.682a6ba0.jpg"
+  },
+  {
+    "revision": "599f1f2e1eb35b682e5d9b7fa54c6148",
+    "url": "./static/media/5.599f1f2e.jpg"
+  },
+  {
+    "revision": "adc107f314ccff86160bd5019c8f7487",
+    "url": "./static/media/iconfont.adc107f3.eot"
+  },
+  {
+    "revision": "4e2a4d1348cca26ddf25193f723571ef",
+    "url": "./static/media/iconfont.4e2a4d13.ttf"
+  },
+  {
+    "revision": "cab162c2b3c2f2a6f0b664abc2613bc0",
+    "url": "./static/media/iconfont.cab162c2.svg"
+  },
+  {
+    "revision": "79dd1c872ef3a8ca4fb425eb0358348d",
+    "url": "./index.html"
+  }
+];
